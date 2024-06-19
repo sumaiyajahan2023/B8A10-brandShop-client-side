cf98508099f0fbacb6bc38573a33ae004d4d955d
sumaiya <sumaiya.cse20@gmail.com>
2024-06-19 14:38:16 +0600

    project setup created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Practise</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
