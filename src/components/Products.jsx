e8dc8d461f95a82a5023fe77a81b8c358115bba8
sumaiya <sumaiya.cse20@gmail.com>
2024-06-20 12:30:57 +0600

    delete operation completed

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -8,9 +8,18 @@ export default function Products() {
       .then((res) => res.json())
       .then((data) => {
         setProducts(data);
-        console.log(data);
+        // console.log(data);
       });
-  }, []);
+  }, [products]);
+
+  const handleDeleteClick = (_id) => {
+    console.log(_id)
+    fetch(`http://localhost:5000/products/${_id}`,{
+      method: 'DELETE'
+    })
+    .then(res=>res.json())
+    .then(data=>console.log(data))
+  }
   return (
     <div className="flex flex-col items-center justify-center p-5">
       <h2 className="text-5xl font-bold">See our product catalog!</h2>
@@ -29,7 +38,11 @@ export default function Products() {
               <div className="card-actions justify-end items-center">
                 <div className="badge badge-outline">{product.brandName}</div>
                 <div className="badge badge-outline">{product.price}$</div>
-                <button className="btn btn-primary">Buy Now</button>
+              </div>
+              <div className="card-actions justify-around items-center">
+                <button className="btn btn-primary">Details</button>
+                <button onClick={()=>{handleUpdateClick(product._id)}} className="btn btn-primary">Update</button>
+                <button onClick={()=>{handleDeleteClick(product._id)}} className="btn btn-primary">Delete</button>
               </div>
             </div>
           </div>
